c87968d6dc964afffdb4c12091c628c9848777dc
cesurkedi <cesurkedixx@protonmail.com>
2022-08-31 17:16:56 +0300

    202-08-31

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace backend_netcore
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+          // Try-Catch  
+            try
+            {
+            Console.WriteLine("Sayı Giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girilen sayı:" + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata : " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("İşlem tamamlandı.");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+            
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.Write("İşlem tamamlandı.");
+            }
+
+        }
+    }
+}
